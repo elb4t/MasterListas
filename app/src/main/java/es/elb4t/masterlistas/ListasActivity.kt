492c2e74ebd5d8e8d1d6705d1ed10dbe1c013e3b
Elb4t <eloy4t@gmail.com>
2017-10-29 19:18:45 +0100

    Creación de la aplicación MasterListas

---
+++ app/src/main/java/es/elb4t/masterlistas/ListasActivity.kt
@@ -0,0 +1,12 @@
+package es.elb4t.masterlistas
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listas)
+    }
+}

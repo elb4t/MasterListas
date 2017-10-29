0e13f9daf48db7a28e32ac9fdbeea8879f7e98ad
Elb4t <eloy4t@gmail.com>
2017-10-29 19:55:21 +0100

    Mostrar un mensaje con Snackbar

--- app/src/main/java/es/elb4t/masterlistas/ListasActivity.kt
+++ app/src/main/java/es/elb4t/masterlistas/ListasActivity.kt
@@ -2,11 +2,22 @@ package es.elb4t.masterlistas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.FloatingActionButton
+import android.view.View
+import kotlinx.android.synthetic.main.activity_listas.*
+import kotlinx.android.synthetic.main.activity_listas.view.*
+
 
 class ListasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listas)
+
+        fab.setOnClickListener(View.OnClickListener { view ->
+            Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show()
+        })
+
     }
 }

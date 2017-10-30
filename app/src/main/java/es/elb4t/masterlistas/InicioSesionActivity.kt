e35f82585f013e040c4856130365cc5ad054cf5a
Elb4t <eloy4t@gmail.com>
2017-10-30 18:33:34 +0100

    Uso de Checkbox

--- app/src/main/java/es/elb4t/masterlistas/InicioSesionActivity.kt
+++ app/src/main/java/es/elb4t/masterlistas/InicioSesionActivity.kt
@@ -2,11 +2,20 @@ package es.elb4t.masterlistas
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_inicio_sesion.*
+
 
 class InicioSesionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
+    }
+
+    fun loguearCheckbox(v: View) {
+        val s = "Recordar datos de usuario: " + if (recordarme.isChecked) "Sí" else "No"
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show()
     }
 }

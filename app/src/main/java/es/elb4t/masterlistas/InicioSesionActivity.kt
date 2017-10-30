079f1049b197b140c1db87bdc3cc824c045ce97b
Elb4t <eloy4t@gmail.com>
2017-10-30 18:22:34 +0100

    Creación de la pantalla de inicio de sesión

---
+++ app/src/main/java/es/elb4t/masterlistas/InicioSesionActivity.kt
@@ -0,0 +1,12 @@
+package es.elb4t.masterlistas
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class InicioSesionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}

a50ff735be7f14809b3c96cd19acb2b50cece17a
Elb4t <eloy4t@gmail.com>
2017-10-30 18:42:48 +0100

    Mostrar/ocultar caracteres en campo de contraseña

--- app/src/main/java/es/elb4t/masterlistas/InicioSesionActivity.kt
+++ app/src/main/java/es/elb4t/masterlistas/InicioSesionActivity.kt
@@ -2,6 +2,7 @@ package es.elb4t.masterlistas
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_inicio_sesion.*
@@ -18,4 +19,13 @@ class InicioSesionActivity : AppCompatActivity() {
         val s = "Recordar datos de usuario: " + if (recordarme.isChecked) "Sí" else "No"
         Toast.makeText(this, s, Toast.LENGTH_LONG).show()
     }
+    fun mostrarContraseña(v: View) {
+        if (mostrar_contrasena.isChecked) {
+            contrasena.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
+
+        } else {
+            contrasena.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+    }
 }
+

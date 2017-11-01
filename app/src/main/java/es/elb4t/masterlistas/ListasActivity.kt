765c1ce194954059a69c29e970308b53a8235eb2
Elb4t <eloy4t@gmail.com>
2017-11-01 12:00:14 +0100

    Añadir un Navigation Drawer en MasterListas

--- app/src/main/java/es/elb4t/masterlistas/ListasActivity.kt
+++ app/src/main/java/es/elb4t/masterlistas/ListasActivity.kt
@@ -1,18 +1,24 @@
 package es.elb4t.masterlistas
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import es.elb4t.masterlistas.R.id.*
 import kotlinx.android.synthetic.main.activity_listas.*
+import kotlinx.android.synthetic.main.content_listas.*
 
 
+class ListasActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-
-class ListasActivity : AppCompatActivity() {
 
     private lateinit var adapter: RecyclerView.Adapter<*>
     private lateinit var lManager: RecyclerView.LayoutManager
@@ -20,6 +26,16 @@ class ListasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listas)
+
+        // Toolbar
+        setSupportActionBar(detail_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Navigation drawer
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, detail_toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
 
         fab.setOnClickListener(View.OnClickListener { view ->
             Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show()
@@ -46,5 +62,27 @@ class ListasActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "" + position, Toast.LENGTH_SHORT).show()
             }
         }))
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            nav_1 -> {
+                Toast.makeText(this, "Opción 1",Toast.LENGTH_SHORT).show()
+            }
+            nav_2 -> Toast.makeText(this, "opción 2",Toast.LENGTH_SHORT).show()
+            nav_3 -> Toast.makeText(this, "Opción 3",Toast.LENGTH_SHORT).show()
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
